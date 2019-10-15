9e2fdcfe058f8c747ddf449feff804b9a283030d
john.nordqvist@gmail.com <YY84XLzErkrPSQE>
2019-10-15 22:10:33 +0300

    Updated Docker configurations to be able to run out-of-the-box.

--- config/database.php
+++ config/database.php
@@ -1,10 +1,11 @@
 <?php
 
 /**
- * Enter Your database credentials here.
+ * Enter Your database credentials here. If You use the default docker-compose setup, be sure to use the same
+ * values here and in the /docker/.env file. Never use the default name and password in production!
  */
 
-define('DB_HOST', 'localhost');
-define('DB_USER', 'john');
-define('DB_PASS', 'derjontte');
+define('DB_HOST', 'mysql');
+define('DB_USER', 'user');
+define('DB_PASS', 'pass');
 define('DB_NAME', 'johana');
